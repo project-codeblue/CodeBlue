6ad6b122a511c60fb8a53f00f66f4a9a086b7ff6
backEndKwon <rhaehfdl0433@gmail.com>
2023-05-29 18:47:25 +0900

    feat: Kakao Map API기반 병원 조회: 권용재

--- src/hospitals/controller/hospitals.controller.ts
+++ src/hospitals/controller/hospitals.controller.ts
@@ -2,21 +2,13 @@ import { Controller, Get } from '@nestjs/common';
 import { HospitalsService } from '../service/hospitals.service';
 import { Logger } from '@nestjs/common';
 import { Hospitals } from '../hospitals.entity';
-
 @Controller('hospital')
 export class HospitalsController {
   private logger = new Logger('HospitalsController');
   constructor(private hospitalsService: HospitalsService) {}
-
   @Get()
   getHospitals(): Promise<Hospitals[]> {
     this.logger.verbose('Getting all hospitals');
     return this.hospitalsService.getHospitals();
   }
-
-  @Get('/crawl')
-  getNearByHospitals(): Promise<string[]> {
-    this.logger.verbose('Getting nearby hospitals');
-    return this.hospitalsService.getNearByHospitals();
-  } 
 }
--- src/hospitals/hospitals.repository.ts
+++ src/hospitals/hospitals.repository.ts
@@ -2,11 +2,17 @@ import { Repository, DataSource } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { Hospitals } from './hospitals.entity';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import { Reports } from 'src/reports/reports.entity';
+
 const DEFAULT_AVAILABLE_BEDS = 5;
 
 @Injectable()
 export class HospitalsRepository extends Repository<Hospitals> {
-  constructor(private dataSource: DataSource) {
+  constructor(
+    private dataSource: DataSource,
+    private reportsRepository: Repository<Reports>,
+    private hospitalsRepository: Repository<Hospitals>,
+  ) {
     super(Hospitals, dataSource.createEntityManager());
   }
 
@@ -34,5 +40,18 @@ export class HospitalsRepository extends Repository<Hospitals> {
       hospital.available_beds = DEFAULT_AVAILABLE_BEDS;
       await this.save(hospital);
     });
+  }
+
+  async userLocation(report_id: number): Promise<number[]> {
+    //사용자 위치(단일)
+    const reports: Reports = (await this.reportsRepository.find()).find(
+      (data) => data.report_id === report_id,
+    );
+    return [reports.latitude, reports.longitude];
+  }
+
+  async AllHospitals(): Promise<Hospitals[]> {
+    //병원 위치(복수)
+    return this.hospitalsRepository.find();
   }
 }
