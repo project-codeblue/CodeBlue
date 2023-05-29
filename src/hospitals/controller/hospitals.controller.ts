fcd24eec039663f0abad76a1cf0384eff456b93f
backEndKwon <rhaehfdl0433@gmail.com>
2023-05-29 19:18:06 +0900

    Kakao Map API기반 병원 조회: 권용재

--- src/hospitals/controller/hospitals.controller.ts
+++ src/hospitals/controller/hospitals.controller.ts
@@ -1,14 +1,27 @@
 import { Controller, Get } from '@nestjs/common';
 import { HospitalsService } from '../service/hospitals.service';
-import { Logger } from '@nestjs/common';
+import { Logger, Param } from '@nestjs/common';
 import { Hospitals } from '../hospitals.entity';
 @Controller('hospital')
 export class HospitalsController {
   private logger = new Logger('HospitalsController');
   constructor(private hospitalsService: HospitalsService) {}
+  
   @Get()
   getHospitals(): Promise<Hospitals[]> {
     this.logger.verbose('Getting all hospitals');
     return this.hospitalsService.getHospitals();
+  }
+
+  @Get('/crawl')
+  getNearByHospitals(): Promise<string[]> {
+    this.logger.verbose('Getting nearby hospitals');
+    return this.hospitalsService.getNearByHospitals();
+  } 
+ 
+  @Get('/hospital/:report_id')
+  getReccomandHospitals(@Param('report_id') report_id: number): Promise<Hospitals[]> {
+    this.logger.verbose('Getting all hospitals');
+    return this.hospitalsService.getReccomandHospitals(report_id);
   }
 }
