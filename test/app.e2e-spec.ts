bdf149962040a54432e8c1ed3074ab28ae0ea9cd
Siwon-Kim <siwonkim1108@gmail.com>
2023-06-02 19:29:25 +0900

    test: e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -8,7 +8,7 @@ import { HttpExceptionFilter } from 'src/commons/exceptions/http-exception.filte
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -31,24 +31,23 @@ describe('AppController (e2e)', () => {
   // 1. 증상 보고서 입력 /report
   // 2. 병원 조회 /hospital
   // 3. 환자 이송 신청 /request/:report_id/:hospital_id
-  // 4. 증상 보고서 환자 정보 업데이트 /report/:report_id
-  // 5. 증상 보고서 검색 및 리스트 조회 /request/search
-  // 6. 증상 보고서 상세 조회 /report/:report_id
+  // 4. 증상 보고서 검색 및 리스트 조회 /request/search
+  // 5. 증상 보고서 상세 조회 /report/:report_id
+  // 6. 증상 보고서 환자 정보 업데이트 /report/:report_id
 
   // 1. 증상 보고서 입력
-  describe('/report (POST)', () => {
-    it('201 증상 보고서 입력 성공', () => {
+  describe('/report', () => {
+    it('201 증상 보고서 입력 성공 (POST)', () => {
       return request(app.getHttpServer())
         .post('/report')
         .send({
-          symptoms: "['두통', '발열', '오한', '피로']",
-          symptom_level: 2,
+          symptoms: '소실된 의식,심부전,사지 마비',
+          name: '김철수',
         })
-        .expect(201)
-        .expect('Hello World!');
+        .expect(201);
     });
 
-    it('412 Validation Error: body data 형식이 맞지 않을 때', () => {
+    it('412 Validation Error: body data 형식이 맞지 않을 때 (POST)', () => {
       return request(app.getHttpServer())
         .post('/report')
         .send({
@@ -60,38 +59,54 @@ describe('AppController (e2e)', () => {
   });
 
   // 2. 병원 조회
-  describe('/hospital (GET)', () => {
-    it('200 병원 조회 성공', () => {
+  describe('/hospital', () => {
+    it('200 병원 조회 성공 (GET)', () => {
       return request(app.getHttpServer()).get('/hospital').expect(200);
     });
   });
 
   // 3. 환자 이송 신청
   describe('/request/:report_id/:hospital_id (POST)', () => {
-    it('201 이송 신청 성공', () => {
+    it('201 이송 신청 성공 (GET)', () => {
       return request(app.getHttpServer()).post('/request/1/1').expect(201);
     });
 
-    it('404 NotFoundException: 해당 병원이 없을 때', () => {
+    it('404 NotFoundException: 해당 병원이 없을 때 (GET)', () => {
       return request(app.getHttpServer()).post('/request/1/100000').expect(404);
     });
 
-    it('404 NotFoundException: 해당 증상 보고서가 없을 때', () => {
+    it('404 NotFoundException: 해당 증상 보고서가 없을 때 (GET)', () => {
       return request(app.getHttpServer()).post('/request/100000/1').expect(404);
     });
 
-    it('400 BAD_REQUEST: 이미 증상 보고서를 전송하였을 때', () => {
+    it('400 BAD_REQUEST: 이미 증상 보고서를 전송하였을 때 (GET)', () => {
       return request(app.getHttpServer()).post('/request/1/1').expect(400);
     });
 
-    it('503 SERVICE_UNAVAILABLE: 해당 병원의 병상이 다 찼을 때', () => {
+    it('503 SERVICE_UNAVAILABLE: 해당 병원의 병상이 다 찼을 때 (GET)', () => {
       return request(app.getHttpServer()).post('/request/1/2').expect(503);
     });
   });
 
-  // 4. 증상 보고서 환자 정보 업데이트
-  describe('/report/:report_id (PATCH)', () => {
-    it('200 환자 정보 업데이트 성공', () => {
+  // 4. 증상 보고서 검색 및 리스트 조회
+  describe('/report/search', () => {
+    it('200 검색 성공 (GET)', () => {
+      return request(app.getHttpServer()).get('/report/search').expect(200);
+    });
+  });
+
+  // 5. 증상보고서 상세 조회
+  // 6. 증상 보고서 환자 정보 업데이트
+  describe('/report/:report_id', () => {
+    it('200 상세 조회 성공 (GET)', () => {
+      return request(app.getHttpServer()).get('/report/1').expect(200);
+    });
+
+    it('404 NotFoundException: 해당 증상 보고서가 없을 때 (GET)', () => {
+      return request(app.getHttpServer()).get('/report/100000').expect(404);
+    });
+
+    it('200 환자 정보 업데이트 성공 (PATCH)', () => {
       return request(app.getHttpServer())
         .patch('/report/1')
         .send({
@@ -103,26 +118,8 @@ describe('AppController (e2e)', () => {
         .expect(200);
     });
 
-    it('404 NotFoundException: 해당 증상 보고서가 없을 때', () => {
+    it('404 NotFoundException: 해당 증상 보고서가 없을 때 (PATCH)', () => {
       return request(app.getHttpServer()).patch('/request/100000').expect(404);
-    });
-  });
-
-  // 5. 증상 보고서 검색 및 리스트 조회
-  describe('/report/search (GET)', () => {
-    it('200 검색 성공', () => {
-      return request(app.getHttpServer()).get('/report/search').expect(200);
-    });
-  });
-
-  // 6. 증상보고서 상세 조회
-  describe('/report/:report_id (GET)', () => {
-    it('200 상세 조회 성공', () => {
-      return request(app.getHttpServer()).get('/report/1').expect(200);
-    });
-
-    it('404 NotFoundException: 해당 증상 보고서가 없을 때', () => {
-      return request(app.getHttpServer()).get('/report/100000').expect(404);
     });
   });
 
