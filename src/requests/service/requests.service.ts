e7886ec86ba9abc808f4cecaab9f1eec1895e3cf
ksnx3684 <ksnx3684@naver.com>
2023-06-03 00:34:19 +0900

    feat: 검색 결과 수정 및 에러 처리 적용

--- src/requests/service/requests.service.ts
+++ src/requests/service/requests.service.ts
@@ -25,59 +25,77 @@ export class RequestsService {
   }
 
   async getSearchRequests(queries: object): Promise<Reports[]> {
-    const query = this.reportsRepository
-      .createQueryBuilder('reports')
-      .leftJoinAndSelect('reports.hospital', 'hospital')
-      .where('1 = 1')
-      .where('is_sent = 1');
+    try {
+      const query = this.reportsRepository
+        .createQueryBuilder('reports')
+        // .leftJoinAndSelect('reports.hospital', 'hospital')
+        .leftJoin('reports.hospital', 'hospital')
+        .select([
+          'reports.report_id', 'reports.name', 'reports.symptom_level', 'reports.symptoms', 'reports.createdAt',
+          'hospital.name', 'hospital.phone', 'hospital.emogList'
+        ])
+        .where('1 = 1')
+        .where('is_sent = 1');
 
-    if (queries['date']) {
-      // URL 쿼리에 날짜가 존재하면 실행
-      const dates: string[] = queries['date'].split('~'); // '~' 를 기준으로 날짜 범위 구분
+      if (queries['date']) {
+        // URL 쿼리에 날짜가 존재하면 실행
+        const dates: string[] = queries['date'].split('~'); // '~' 를 기준으로 날짜 범위 구분
 
-      const regex = /^\d{4}-\d{2}-\d{2}$/;
-      for (const date of dates) {
-        if (!regex.test(date)) {
-          throw new Error('날짜 형식이 맞지 않습니다');
+        const regex = /^\d{4}-\d{2}-\d{2}$/;
+        for (const date of dates) {
+          if (!regex.test(date)) {
+            throw new Error('날짜 형식이 맞지 않습니다');
+          }
         }
+
+        if (dates.length === 1) {
+          const temp = new Date(dates[0]);
+          temp.setDate(temp.getDate() + 1);
+          dates.push(date.format(temp, 'YYYY-MM-DD'));
+        }
+
+        await query.andWhere(
+          new Brackets((qb) => {
+            qb.andWhere(
+              `reports.createdAt BETWEEN '${dates[0]}' AND '${dates[1]}'`,
+            );
+          }),
+        );
       }
 
-      if (dates.length === 1) {
-        const temp = new Date(dates[0]);
-        temp.setDate(temp.getDate() + 1);
-        dates.push(date.format(temp, 'YYYY-MM-DD'));
+      if (queries['symptoms']) {
+        // URL 쿼리에 증상이 존재하면 실행
+        const symptoms = queries['symptoms'].split(','); // 쉼표를 기준으로 증상 구분
+        symptoms.forEach((symptom: string) => {
+          query.andWhere(`reports.symptoms LIKE '%${symptom}%'`);
+        });
       }
 
-      await query.andWhere(
-        new Brackets((qb) => {
-          qb.andWhere(
-            `reports.createdAt BETWEEN '${dates[0]}' AND '${dates[1]}'`,
-          );
-        }),
-      );
+      if (queries['symptom_level']) {
+        // URL 쿼리에 증상도가 존재하면 실행
+        query.andWhere(`reports.symptom_level = ${queries['symptom_level']}`);
+      }
+
+      if (queries['site']) {
+        // URL 쿼리에 지역이 존재하면 실행
+        query.andWhere(`hospital.address LIKE '%${queries['site']}%'`);
+      }
+
+      const allReports = await query.getRawMany();
+      // const allReports = query.getMany();
+
+      if (allReports.length === 0) {
+        throw new NotFoundException;
+      }
+
+      return allReports;
+    } catch (error) {
+      if (error.response.statusCode === 404) {
+        throw new NotFoundException('검색 결과가 없습니다');
+      } else {
+        throw new Error('검색 조회에 실패하였습니다.');
+      }
     }
-
-    if (queries['symptoms']) {
-      // URL 쿼리에 증상이 존재하면 실행
-      const symptoms = queries['symptoms'].split(','); // 쉼표를 기준으로 증상 구분
-      symptoms.forEach((symptom: string) => {
-        query.andWhere(`reports.symptoms LIKE '%${symptom}%'`);
-      });
-    }
-
-    if (queries['symptom_level']) {
-      // URL 쿼리에 증상도가 존재하면 실행
-      query.andWhere(`reports.symptom_level = ${queries['symptom_level']}`);
-    }
-
-    if (queries['site']) {
-      // URL 쿼리에 지역이 존재하면 실행
-      query.andWhere(`hospital.address LIKE '%${queries['site']}%'`);
-    }
-
-    const allReports = query.getRawMany();
-
-    return allReports;
   }
 
   async createRequest(report_id: number, hospital_id: number) {
