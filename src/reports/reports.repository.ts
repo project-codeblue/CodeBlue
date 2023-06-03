484966b1a754ba350a3a99df3b720e12fbc2dc18
ksnx3684 <ksnx3684@naver.com>
2023-06-03 16:09:26 +0900

    fix: hospitals Spatial Index 적용에 따른 repository 쿼리 변경

--- src/reports/reports.repository.ts
+++ src/reports/reports.repository.ts
@@ -20,10 +20,17 @@ export class ReportsRepository extends Repository<Reports> {
   }
 
   async getReportDetails(report_id): Promise<Reports> {
-    return await this.findOne({
+    const results = await this.findOne({
       where: { report_id },
       relations: ['hospital'],
     }).then((report) => {
+      // const results = await this.query(
+      //   `
+      //     SELECT * FROM reports r LEFT JOIN hospitals h
+      //     ON r.hospital_id = h.hospital_id
+      //     WHERE r.report_id = ${report_id};
+      //   `
+      // ).then((report) => {
       const { hospital_id, name, address, phone } = report.hospital;
       const transformedReport = Object.assign(new Reports(), report);
       transformedReport.hospital = {
@@ -34,6 +41,7 @@ export class ReportsRepository extends Repository<Reports> {
       } as Hospitals;
       return transformedReport;
     });
+    return results;
   }
 
   async findReport(report_id: number): Promise<Reports> {
