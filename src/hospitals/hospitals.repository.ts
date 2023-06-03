f68da5c04f805c29fefc96312f97dd6ff9716059
ksnx3684 <ksnx3684@naver.com>
2023-06-03 10:44:04 +0900

    !BREAKING CHANGE: hospitals 엔티티 변경 및 spatial Index 수정

--- src/hospitals/hospitals.repository.ts
+++ src/hospitals/hospitals.repository.ts
@@ -44,9 +44,9 @@ export class HospitalsRepository extends Repository<Hospitals> {
   ) {
     return await this.query(
       `
-        SELECT geo_id, name, phone, available_beds, latitude, longitude, emogList, ST_Distance_Sphere(Point(${startLng}, ${startLat}),
+        SELECT hospital_id, name, phone, available_beds, latitude, longitude, emogList, ST_Distance_Sphere(Point(${startLng}, ${startLat}),
         point) as 'distance'
-        FROM geohospital
+        FROM hospitals
         WHERE ST_Distance_Sphere(POINT(${startLng}, ${startLat}), point) < (${radius})
         order by distance;
       `,
@@ -56,9 +56,9 @@ export class HospitalsRepository extends Repository<Hospitals> {
   async getHospitalsWithoutRadius(startLng: number, startLat: number) {
     return await this.query(
       `
-          SELECT geo_id, name, phone, available_beds, latitude, longitude, emogList, ST_Distance_Sphere(Point(${startLng}, ${startLat}),
+          SELECT hospital_id, name, phone, available_beds, latitude, longitude, emogList, ST_Distance_Sphere(Point(${startLng}, ${startLat}),
           point) as 'distance'
-          FROM geohospital
+          FROM hospitals
           order by distance;
       `,
     );
