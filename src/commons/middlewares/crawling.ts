04a866b6b2365d17e9a9c08d4c107d38179c0a8c
ksnx3684 <ksnx3684@naver.com>
2023-06-06 23:21:08 +0900

    feat: 네이버 지식인 크롤링 API 추가

--- src/commons/middlewares/crawling.ts
+++ src/commons/middlewares/crawling.ts
@@ -2,6 +2,8 @@ import puppeteer from 'puppeteer';
 import * as cheerio from 'cheerio';
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Injectable()
 export class Crawling {
@@ -88,16 +90,16 @@ export class Crawling {
         const $data = cheerio.load(element);
         const emogList = $data('#area_dashboards > div > div.dash_header > div > span > input.emogcode',).val();
         const name = $data('#area_dashboards > div > div.dash_header > div > span > a',).text().replace(/\s+/g, ' ');
-        const ungeup = $data('#area_dashboards > div > div.dash_data > div:nth-child(2) > table > tbody > tr > td:nth-child(1) > div.data_data.emer_bed.data_td_O001 > span:nth-child(2)',).text().replace(/\s+/g, ' ');
-        const susul = $data(`#rltmList_${idx} > table > tbody > tr:nth-child(2) > td:nth-child(2) > div.data_data.data_td_O022`,).text().replace(/\s+/g, ' ');
-        const singyeong = $data(`#rltmList_${idx} > table > tbody > tr:nth-child(1) > td:nth-child(4) > div.data_data.data_td_O011`,).text().replace(/\s+/g, ' ');
-        const sinseng = $data(`#rltmList_${idx} > table > tbody > tr:nth-child(1) > td:nth-child(2) > div.data_data.data_td_O008`,).text().replace(/\s+/g, ' ');
-        const hyungbu = $data(`#rltmList_${idx} > table > tbody > tr:nth-child(1) > td:nth-child(7) > div.data_data.data_td_O016`,).text().replace(/\s+/g, ' ');
-        const ilban = $data(`#rltmList_${idx} > table > tbody > tr:nth-child(1) > td:nth-child(1) > div.data_data.data_td_O017`,).text().replace(/\s+/g, ' ');
-        const ibwon = $data(`#rltmList_${idx} > table > tbody > tr:nth-child(2) > td:nth-child(1) > div.data_data.data_td_O038`,).text().replace(/\s+/g, ' ');
-        const naeggwa = $data(`#rltmList_${idx} > table > tbody > tr:nth-child(1) > td:nth-child(3) > div.data_data.data_td_O006`,).text().replace(/\s+/g, ' ');
-        const weggwa = $data(`#rltmList_${idx} > table > tbody > tr:nth-child(1) > td:nth-child(5) > div.data_data.data_td_O007`,).text().replace(/\s+/g, ' ');
-        const singyeongwe = $data(`#rltmList_${idx} > table > tbody > tr:nth-child(1) > td:nth-child(6) > div.data_data.data_td_O012`,).text().replace(/\s+/g, ' ');
+        const ungeup = $data('#area_dashboards > div > div.dash_data > div:nth-child(2) > table > tbody > tr > td:nth-child(1) > div.data_data.emer_bed.data_td_O001 > span:nth-child(2)').text().replace(/\s+/g, ' ');
+        const susul = $data(`#rltmList_${idx} > table > tbody > tr:nth-child(2) > td:nth-child(2) > div.data_data.data_td_O022`).text().replace(/\s+/g, ' ');
+        const singyeong = $data(`#rltmList_${idx} > table > tbody > tr:nth-child(1) > td:nth-child(4) > div.data_data.data_td_O011`).text().replace(/\s+/g, ' ');
+        const sinseng = $data(`#rltmList_${idx} > table > tbody > tr:nth-child(1) > td:nth-child(2) > div.data_data.data_td_O008`).text().replace(/\s+/g, ' ');
+        const hyungbu = $data(`#rltmList_${idx} > table > tbody > tr:nth-child(1) > td:nth-child(7) > div.data_data.data_td_O016`).text().replace(/\s+/g, ' ');
+        const ilban = $data(`#rltmList_${idx} > table > tbody > tr:nth-child(1) > td:nth-child(1) > div.data_data.data_td_O017`).text().replace(/\s+/g, ' ');
+        const ibwon = $data(`#rltmList_${idx} > table > tbody > tr:nth-child(2) > td:nth-child(1) > div.data_data.data_td_O038`).text().replace(/\s+/g, ' ');
+        const naeggwa = $data(`#rltmList_${idx} > table > tbody > tr:nth-child(1) > td:nth-child(3) > div.data_data.data_td_O006`).text().replace(/\s+/g, ' ');
+        const weggwa = $data(`#rltmList_${idx} > table > tbody > tr:nth-child(1) > td:nth-child(5) > div.data_data.data_td_O007`).text().replace(/\s+/g, ' ');
+        const singyeongwe = $data(`#rltmList_${idx} > table > tbody > tr:nth-child(1) > td:nth-child(6) > div.data_data.data_td_O012`).text().replace(/\s+/g, ' ');
 
         results.push(
           `${emogList} / ${name} 응급실: ${ungeup}, 수술실: ${susul}, 신경중환자실: ${singyeong}, 신생아중환자실: ${sinseng}, 흉부중환자실: ${hyungbu}, 일반중환자실: ${ilban}, 입원실: ${ibwon}, 내과중환자실: ${naeggwa}, 외과중환자실: ${weggwa}, 신경외과중환자실: ${singyeongwe}`,
@@ -110,5 +112,62 @@ export class Crawling {
     console.log(`크롤링 소요시간 : ${t}ms`);
 
     return results;
+  }
+
+  // 네이버 지식인 크롤링
+  async symptomCrawl() {
+    const start: any = new Date();
+
+    // 브라우저 실행
+    const browser = await puppeteer.launch({ headless: false });
+
+    // 페이지 생성
+    const page = await browser.newPage();
+
+    // 크롤링할 대상 사이트로 이동
+    await page.goto('https://kin.naver.com/qna/expertAnswerList.naver?dirId=701');
+
+    let data: string = '';
+
+    for (let i = 1; i <= 20; i++) {
+      await page.waitForSelector(`#au_board_list > tr:nth-child(${i}) > td.title > a`);
+      await page.click(`#au_board_list > tr:nth-child(${i}) > td.title > a`);
+      await page.waitForSelector(`#wrap > div.container-fluid`);
+      // 데이터 가져오기
+      const content = await page.content();
+      const $ = cheerio.load(content);
+      const qtitle = $('#content > div.question-content > div > div.c-heading._questionContentsArea.c-heading--default-old > div.c-heading__title > div > div').text().replace(/\s+/g, ' ');
+      const qcontent = $('#content > div.question-content > div > div.c-heading._questionContentsArea.c-heading--default-old > div.c-heading__content').text().replace(/\s+/g, ' ');
+      const acontent = $('#answer_1 > div._endContents.c-heading-answer__content > div._endContentsText.c-heading-answer__content-user').text().replace(/\s+/g, ' ');
+      
+      data += '-------------------질문-----------------------------\n';
+      data += qtitle + '\n';
+      data += qcontent + '\n';
+      data += '-------------------답변-----------------------------\n'
+      data += acontent + '\n\n\n';
+
+      await page.goBack();
+    }
+    
+    // Close browser.
+    await browser.close();
+
+    const files = fs.readdirSync('./crawl');
+
+    const fileList = files.filter(file => file.startsWith('file'));
+
+    if (fileList.length > 0) {
+      const fileNames = fileList.map(file => file.split('.')[0]);
+
+      const fileNumbers = fileNames.map(file => +file.split('file')[1]);
+
+      fileNumbers.sort((a, b) => b - a);
+
+      const max = fileNumbers[0];
+
+      fs.writeFileSync(`./crawl/file${max+1}.txt`, data, 'utf-8');
+    } else {
+      fs.writeFileSync(`./crawl/file1.txt`, data, 'utf-8');
+    }
   }
 }
