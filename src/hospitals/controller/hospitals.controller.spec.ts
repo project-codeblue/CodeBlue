5a5bccf747573df1d0fe673ac4d0f4057d03d57b
ksnx3684 <ksnx3684@naver.com>
2023-06-29 09:13:36 +0900

    test: hospitals controller unit test

--- src/hospitals/controller/hospitals.controller.spec.ts
+++ src/hospitals/controller/hospitals.controller.spec.ts
@@ -1,53 +1,60 @@
 import { Test } from '@nestjs/testing';
 import { HospitalsController } from './hospitals.controller';
 import { HospitalsService } from '../service/hospitals.service';
+import { CacheInterceptor } from '../../commons/interceptors/cache.interceptor';
+import { CACHE_MANAGER, CacheModule } from '@nestjs/cache-manager';
 
 describe('HospitalsController Unit Testing', () => {
   let hospitalsController;
   let hospitalsService;
+  let interceptor: CacheInterceptor;
 
   beforeEach(async () => {
     const mockHospitalsService = {
-      getLocalHospitals: jest.fn().mockReturnValue({}),
       getRecommendedHospitals: jest.fn().mockReturnValue({}),
+      getNearbyHospitals: jest.fn().mockReturnValue({}),
     };
 
     const moduleRef = await Test.createTestingModule({
+      imports: [CacheModule.register()],
       controllers: [HospitalsController],
       providers: [
         { provide: HospitalsService, useValue: mockHospitalsService },
+        { provide: CACHE_MANAGER, useValue: {} },
+        CacheInterceptor
       ],
     }).compile();
 
     hospitalsController = moduleRef.get(HospitalsController);
     hospitalsService = moduleRef.get(HospitalsService);
-  });
-
-  describe('getLocalHospitals()', () => {
-    it('should get local Hospitals', async () => {
-      const localHospitals = [];
-      jest
-        .spyOn(hospitalsService, 'getLocalHospitals')
-        .mockImplementation(() => localHospitals);
-
-      expect(await hospitalsController.getLocalHospitals()).toBe(
-        localHospitals,
-      );
-      expect(hospitalsService.getLocalHospitals).toBeCalledTimes(1);
-    });
+    interceptor = moduleRef.get(CacheInterceptor);
   });
 
   describe('getRecommendedHospitals()', () => {
     it('should get recommended Hospitals', async () => {
-      const recommendedHospitals = [];
+      const recommendedHospitals = { "hospitals_data": {} };
       jest
         .spyOn(hospitalsService, 'getRecommendedHospitals')
-        .mockImplementation(() => recommendedHospitals);
+        // .mockImplementation(() => recommendedHospitals);
 
-      expect(await hospitalsController.getRecommendedHospitals()).toBe(
+      expect(await hospitalsController.getRecommendedHospitals()).toEqual(
         recommendedHospitals,
       );
       expect(hospitalsService.getRecommendedHospitals).toBeCalledTimes(1);
+    });
+  });
+
+  describe('getNearbyHospitals()', () => {
+    it('should get nearby Hospitals', async () => {
+      const localHospitals = { "hospitals_data": {} };
+      jest
+        .spyOn(hospitalsService, 'getNearbyHospitals')
+        // .mockImplementation(() => localHospitals);
+
+      expect(await hospitalsController.getNearbyHospitals()).toEqual(
+        localHospitals,
+      );
+      expect(hospitalsService.getNearbyHospitals).toBeCalledTimes(1);
     });
   });
 });
